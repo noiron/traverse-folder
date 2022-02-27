877fb41376eaac700255fcb6d251a6df4f538e66
wu kai <wukai2048@gmail.com>
2022-02-27 11:50:07 +0800

    test action

--- traverseFolderList.js
+++ traverseFolderList.js
@@ -11,6 +11,7 @@ const traverseFolderList = (rootPath, list) => {
   const files = fs.readdirSync(rootPath);
 
 
+  
   files.forEach((file) => {
     const absolutePath = path.resolve(rootPath, file);
     const stats = fs.statSync(absolutePath);
