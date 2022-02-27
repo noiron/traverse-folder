f306337a9932c4f519dc62c9e87ef07fdeecb8f6
wu kai <wukai2048@gmail.com>
2022-02-27 19:17:27 +0800

    update 测试文件

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const path = require('path');
 const { traverseFolderList } = require('./traverseFolderList');
 const { asyncTraverseFolderList } = require('./asyncTraverseFolderList');
 
+// 可以任选一个文件夹用于测试
 const folderPath = path.resolve(os.homedir(), 'code');
 
 function test1() {
@@ -10,28 +11,30 @@ function test1() {
   const startTime = Date.now();
   traverseFolderList(folderPath, list);
   const endTime = Date.now();
-  console.log(`共${list.length}个文件, 耗时${endTime - startTime}ms`);
-  return endTime - startTime;
+  const duration = endTime - startTime;
+  console.log(`同步 - 共${list.length}个文件, 耗时${duration}ms`);
+  return duration;
 }
-
-let time1 = 0;
-for (let i = 0; i < 10; i++) {
-  time1 += test1();
-}
-console.log(`平均耗时：${time1 / 10}ms`);
 
 async function test2() {
   const startTime = Date.now();
   const list = await asyncTraverseFolderList(folderPath);
   const endTime = Date.now();
-  console.log(`共${list.length}个文件, 耗时${endTime - startTime}ms`);
-  return endTime - startTime;
+  const duration = endTime - startTime;
+  console.log(`异步 - 共${list.length}个文件, 耗时${duration}ms`);
+  return duration;
+}
+
+async function runTest(fn, count) {
+  let time = 0;
+  for (let i = 0; i < count; i++) {
+    time += await fn();
+  }
+  console.log(`${fn.name} 共运行${count}次，平均耗时：${time / count}ms`);
 }
 
 (async function () {
-  let time2 = 0;
-  for (let i = 0; i < 10; i++) {
-    time2 += await test2();
-  }
-  console.log(`平均耗时：${time2 / 10}ms`);
+  const COUNT = 10;
+  await runTest(test1, COUNT);
+  await runTest(test2, COUNT);
 })();
